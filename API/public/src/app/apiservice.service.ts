6ad7bde1c9a0f8c2611d596acd36636e45034e09
jhonvo <jhon26victor@gmail.com>
2021-12-01 11:21:38 -0500

    API Updarte

--- API/public/src/app/apiservice.service.ts
+++ API/public/src/app/apiservice.service.ts
@@ -14,12 +14,15 @@ export class ApiserviceService {
     // let habilityData : any = this.habilityShare(pokemonData.abilities[0].ability.url);
   }
 
-  getPokemon(id : number ){
+  getPokemon2(){
+    return this._http.get('https://pokeapi.co/api/v2/pokemon/4');
+  }
+
+  getPokemon(id : number ) : any{
     let charmander = this._http.get('https://pokeapi.co/api/v2/pokemon/'+id);
     charmander.subscribe((data : any): any => {
-      // console.log(data.abilities);
-      this.pokemonInfo = data;
       console.log(`The pokemon name is ${data.forms[0].name}, it is a ${data.types[0].type.name} pokemon and its abilities are ${data.abilities[0].ability.name} and ${data.abilities[1].ability.name}.` );
+            
       for (var i : number  = 0; i < data.abilities.length; i++){
         this.habilityShare(data.abilities[i].ability.url);
       }
--- API/public/src/app/app.component.ts
+++ API/public/src/app/app.component.ts
@@ -6,10 +6,26 @@ import { ApiserviceService } from './apiservice.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  pokemon: any ;
   title = 'public';
   pokemonInfo : any;
   constructor(private _apiserviceService: ApiserviceService){
     this.pokemonInfo = _apiserviceService.pokemonInfo;
     console.log("THIS IS FROM THE COMPONENT" + this.pokemonInfo);
   }
+
+  ngOnInit(): void {
+      console.log("Before loading pokemons: ", this.pokemon);
+      let bulbasaur = this._apiserviceService.getPokemon2();
+      bulbasaur.subscribe(
+        (data:any) =>{
+          console.log("estos es data: "+data)
+          this.pokemon=data
+          console.log("Este es Bulbasaur: "+this.pokemon.abilities[0])
+          console.log("esta es la primera habilidad de Bulbasaur: "+this.pokemon.abilities[0].ability.name)
+        },
+        (error:any) =>{
+          console.log("something went wrong", error);
+        })
+}
 }
